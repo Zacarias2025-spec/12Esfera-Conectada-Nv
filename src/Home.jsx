84f4a6ee433c903ee6a3ce77800e489796772be0
Zacarias2025-spec <saramendes.ang2025@gmail.com>
2025-11-27 10:21:15 +0100

    Add files via upload

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Feed from '../components/Feed'
+
+export default function Home(){
+  return (
+    <div className="max-w-3xl mx-auto space-y-4">
+      <Feed />
+    </div>
+  )
+}
